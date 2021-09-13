0f899917189cd25d3938bbabe3e818efa8beb2f8
AJWalker2 <34259773+AJWalker2@users.noreply.github.com>
2021-09-13 16:04:58 -0400

    Update Installing.bat

--- Scripts/Installing.bat
+++ Scripts/Installing.bat
@@ -1,26 +1,26 @@
 msiexec -i C:\Users\runneradmin\Documents\Applications\openvpn1.msi /qn:
 explorer C:\Users\runneradmin\Documents\Applications\droidcam.exe
-mv C:\Users\runneradmin\AppData\Local\DiscordCanary C:\Users\runneradmin\DiscordCanary
-rm -rf C:\Users\runneradmin\AppData\Local\DiscordCanary
-mv C:\Users\runneradmin\AppData\Roaming\discordcanary C:\Users\runneradmin\discordcanary2
-rm -rf C:\Users\runneradmin\AppData\Roaming\discordcanary
-mv C:\Users\runneradmin\AppData\Roaming\discord C:\Users\runneradmin\discord
-rm -rf C:\Users\runneradmin\AppData\Roaming\discord
+# mv C:\Users\runneradmin\AppData\Local\DiscordCanary C:\Users\runneradmin\DiscordCanary
+# rm -rf C:\Users\runneradmin\AppData\Local\DiscordCanary
+# mv C:\Users\runneradmin\AppData\Roaming\discordcanary C:\Users\runneradmin\discordcanary2
+# rm -rf C:\Users\runneradmin\AppData\Roaming\discordcanary
+# mv C:\Users\runneradmin\AppData\Roaming\discord C:\Users\runneradmin\discord
+# rm -rf C:\Users\runneradmin\AppData\Roaming\discord
 C:\Users\runneradmin\Documents\Applications\DiscordCanarySetup.exe -s
-rm -rf C:\Users\runneradmin\AppData\Roaming\discord
-rm -rf C:\Users\runneradmin\AppData\Roaming\discordcanary
-rm -rf C:\Users\runneradmin\AppData\Local\DiscordCanary
-mv C:\Users\runneradmin\DiscordCanary C:\Users\runneradmin\AppData\Local\DiscordCanary
-mv C:\Users\runneradmin\discord C:\Users\runneradmin\AppData\Roaming\discord
-mv C:\Users\runneradmin\discordcanary2 C:\Users\runneradmin\AppData\Roaming\discordcanary
-rm -rf C:\Users\runneradmin\DiscordCanary
-rm -rf C:\Users\runneradmin\discordcanary2
+# rm -rf C:\Users\runneradmin\AppData\Roaming\discord
+# rm -rf C:\Users\runneradmin\AppData\Roaming\discordcanary
+# rm -rf C:\Users\runneradmin\AppData\Local\DiscordCanary
+# mv C:\Users\runneradmin\DiscordCanary C:\Users\runneradmin\AppData\Local\DiscordCanary
+# mv C:\Users\runneradmin\discord C:\Users\runneradmin\AppData\Roaming\discord
+# mv C:\Users\runneradmin\discordcanary2 C:\Users\runneradmin\AppData\Roaming\discordcanary
+# rm -rf C:\Users\runneradmin\DiscordCanary
+# rm -rf C:\Users\runneradmin\discordcanary2
 C:\Users\runneradmin\Documents\Applications\vlc-3.0.16-win64.exe /S
 C:\Users\runneradmin\Documents\Applications\npp.8.1.2.Installer.x64.exe /S
 explorer C:\Users\runneradmin\Documents\Applications\womic_driver_setup.exe
 C:\Users\runneradmin\Documents\Applications\WOMicClientSetupV4.7.exe /S
-C:\Users\runneradmin\Documents\Applications\Splashtop_Streamer_Windows_DEPLOY_INSTALLER_v3.4.8.0_YKX3H5S4YWWZ.exe prevercheck /s /i hidewindow=1,confirm_d=0
+# C:\Users\runneradmin\Documents\Applications\Splashtop_Streamer_Windows_DEPLOY_INSTALLER_v3.4.8.0_YKX3H5S4YWWZ.exe prevercheck /s /i hidewindow=1,confirm_d=0
 explorer C:\Users\runneradmin\Documents\Applications\VAC\setup64.exe
 C:\Users\runneradmin\Documents\Applications\icons.exe /SP- /VERYSILENT
-C:\Users\runneradmin\Documents\Applications\WPSOffice_11.2.0.10258.exe /S
-C:\Users\runneradmin\Documents\Applications\ngrok.exe authtoken 1gHLDbVsq9iRmUHbFc2rFadM6xA_7aesN7xKynKtiXFdieY3J
+# C:\Users\runneradmin\Documents\Applications\WPSOffice_11.2.0.10258.exe /S
+C:\Users\runneradmin\Documents\Applications\ngrok.exe authtoken 1y5juyeMVsZ1bh14nt1hIKmpk0n_4WMmsw1HsbEson8DsMDmV
